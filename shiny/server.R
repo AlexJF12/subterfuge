5fabb95f0d1ca19378164516ab6db83476990d1c
AlexJF12 <alexjf12@gmail.com>
2016-10-11 22:12:15 -0400

    Add files via upload

---
+++ shiny/server.R
@@ -0,0 +1,51 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(rvest)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+
+url <- read_html("http://subterfuge-game.com/leaderboards.html")
+
+sub <- url %>%
+  html_nodes(xpath = '/html/body/table') %>%
+  html_table() %>%
+  as.data.frame()
+
+## Data Cleaning / Managing
+
+sub <- select(sub,
+              player = Player,
+              rating = Rating,
+              rated_games = Ratedgames)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+   
+  output$rating_plot <- renderPlot({
+    ggplot(sub, aes(x = rated_games, y = rating)) + 
+      geom_jitter(alpha = 0.25) + 
+      geom_smooth() +
+      scale_y_continuous(breaks = seq(0, 2000, 100)) + 
+      scale_x_continuous(breaks = seq(0, 200, 5)) +
+      labs(title = "Subterfuge \n Individual Rating by Total Rated Games Played",
+           x = "Total Rated Games Played", y = "Rating") + 
+      annotate("text", x = 85, y = 1600, 
+               label = input$playerInput, color = "red", size = 7) +
+      geom_segment(aes(x = 75, y = 1600,
+                       xend = sub$rated_games[sub$player == input$playerInput],
+                       yend = sub$rating[sub$player == input$playerInput]),
+                   color = "red", arrow = arrow()) + 
+      theme_light()
+      
+  })
+  
+})
